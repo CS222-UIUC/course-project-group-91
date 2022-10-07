853ee21b0752e863bc0aa8ceb47eac76608f5b24
Fay Lin <flin24@illinois.edu>
2022-10-07 16:30:55 -0500

    reorganized files but lost styling, need to add back

--- shoe-yelp/src/index.js
+++ shoe-yelp/src/index.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div class = "header">
-          <div class = "logo">
-              <a id = "home" href = "index.html">
-                  <img id= "logo_img" src='shoelogo.png'></img>
-              </a>
-          </div>
-          <div class = "search"><input></input></div>
-          <div class = "menu">
-            <hr></hr>
-              <ul>
-                  <li class = "pages">
-                      <a class = "link" href = "about.html"> 
-                          <h6 class = "label">ABOUT</h6>
-                      </a>
-                  </li>
-                  <li class = "pages">
-                      <a class = "link" href = "forum.html"> 
-                          <h6 class = "label">FORUM</h6>
-                      </a>
-                  </li>
-                  <li class = "pages">
-                      <a class = "link" href = "trending.html"> 
-                          <h6 class = "label">TRENDING</h6>
-                      </a>
-                  </li>
-                  <li class = "pages">
-                      <a class = "link" href = "contact.html"> 
-                          <h6 class = "label">CONTACT</h6>
-                      </a>
-                  </li>
-              </ul>
-              <hr></hr>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+  
+ReactDOM.render(
+<React.StrictMode>
+    <App />
+</React.StrictMode>,
+document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+
+// class App extends Component {
+//   render() {
+//     return (
+      // <div className="App">
+      //   <div class = "header">
+      //     <div class = "logo">
+      //         <a id = "home" href = "index.html">
+      //             <img id= "logo_img" src='shoelogo.png'></img>
+      //         </a>
+      //     </div>
+      //     <div class = "search"><input></input></div>
+      //     <div class = "menu">
+      //       <hr></hr>
+      //         <ul>
+      //             <li class = "pages">
+      //                 <a class = "link" href = "about.html"> 
+      //                     <h6 class = "label">ABOUT</h6>
+      //                 </a>
+      //             </li>
+      //             <li class = "pages">
+      //                 <a class = "link" href = "forum.html"> 
+      //                     <h6 class = "label">FORUM</h6>
+      //                 </a>
+      //             </li>
+      //             <li class = "pages">
+      //                 <a class = "link" href = "trending.html"> 
+      //                     <h6 class = "label">TRENDING</h6>
+      //                 </a>
+      //             </li>
+      //             <li class = "pages">
+      //                 <a class = "link" href = "contact.html"> 
+      //                     <h6 class = "label">CONTACT</h6>
+      //                 </a>
+      //             </li>
+      //         </ul>
+      //         <hr></hr>
+      //     </div>
+      //   </div>
+      // </div>
+//     )
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'))
