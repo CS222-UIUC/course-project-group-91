58f5873a3981a43a9c1e3b08f8b849ffe2ade93a
EmmanB1 <emmanallws@wirelessprv-10-195-143-71.near.illinois.edu>
2022-10-07 23:23:38 -0500

    Updated the file hierarchy for client

---
+++ shoe-yelp/client/src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./shoelogo.png";
+import { Nav, NavLink, NavMenu} 
+    from "./NavbarElements";
+  
+const Navbar = () => {
+  return (
+    <>
+    <a id = "home" href = "/">
+      <img src = {logo} alt = "shoe logo"/>
+    </a>
+    
+    <hr></hr>
+    <Nav>
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            ABOUT
+          </NavLink>
+          <NavLink to="/forum" activeStyle>
+            FORUM
+          </NavLink>
+          <NavLink to="/trending" activeStyle>
+            TRENDING
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            CONTACT
+          </NavLink>
+        </NavMenu>
+      
+      </Nav>
+      <hr></hr>
+      </>
+  );
+};
+  
+export default Navbar;
