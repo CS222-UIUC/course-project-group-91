46bf0d6ad57353dfc28a9b8014658c72865e822c
EmmanB1 <emmanallws@wirelessprv-10-195-143-71.near.illinois.edu>
2022-10-07 21:43:31 -0500

    Initial commit

--- shoe-yelp/src/loginPage/loginPage.js
+++ shoe-yelp/src/loginPage/loginPage.js
@@ -4,20 +4,29 @@ import TextField from "@material-ui/core/TextField"
 const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    return( 
+    return(
         <div>
-            <TextField 
-            value={username}
-            label="Enter your name"
-            onChange={(e) => {
-                setUsername(e.target.value);
-            }}/>
-            <TextField 
-            value={password}
-            label="Enter your password"
-            onChange={(e) => {
-                setPassword(e.target.value);
-            }}/>
+            <div className="mainContainer">
+                <h1> LOGIN IN </h1>
+            </div>
+            <div className="mainContainer">
+                <div>
+                    <TextField 
+                    value={username}
+                    label="Enter your Username"
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}/>
+                </div>
+                <div>
+                    <TextField 
+                    value={password}
+                    label="Enter your password"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}/>
+                </div>
+            </div>
         </div>
     );
 }
