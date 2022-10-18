d3d15667bb9ec0ef4b7601c2c1d5417776f48c15
EmmanB1 <emmanallws@wirelessprv-10-195-143-71.near.illinois.edu>
2022-10-18 16:43:04 -0500

    pull request

--- shoe-yelp/client/src/components/Navbar/index.js
+++ shoe-yelp/client/src/components/Navbar/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import logo from "./shoelogo.png";
-<<<<<<< HEAD
 import "./navbar.css"
-=======
->>>>>>> 58f5873a (Updated the file hierarchy for client)
 import { Nav, NavLink, NavMenu} 
     from "./NavbarElements";
   
 const Navbar = () => {
   return (
     <>
-<<<<<<< HEAD
     <div className="navbarTopContainer">
         <a id = "home" href = "/">
           <img src = {logo} alt = "shoe logo"/>
@@ -36,31 +32,6 @@ const Navbar = () => {
       </NavMenu>
     </Nav>
     <hr></hr>
-=======
-    <a id = "home" href = "/">
-      <img src = {logo} alt = "shoe logo"/>
-    </a>
-    
-    <hr></hr>
-    <Nav>
-        <NavMenu>
-          <NavLink to="/about" activeStyle>
-            ABOUT
-          </NavLink>
-          <NavLink to="/forum" activeStyle>
-            FORUM
-          </NavLink>
-          <NavLink to="/trending" activeStyle>
-            TRENDING
-          </NavLink>
-          <NavLink to="/contact" activeStyle>
-            CONTACT
-          </NavLink>
-        </NavMenu>
-      
-      </Nav>
-      <hr></hr>
->>>>>>> 58f5873a (Updated the file hierarchy for client)
       </>
   );
 };
