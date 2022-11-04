66adc54be223933ebb17cef7cfe602121335be01
Kyle Polson <kylekpolson@gmail.com>
2022-11-04 16:48:25 -0500

    Added password check

--- shoe-yelp/server/mongo/test.js
+++ shoe-yelp/server/mongo/test.js
@@ -1,11 +1,17 @@
 const { user_profile_collection, closeConnection, mongo_client } = require('./db.js');
 const db = require('./db.js')
 
+/**
+ * Checks if jboat is a username in user_profile_collection and gets the password for that user.
+ */
 async function run() {
     try {
       const valid = await db.isValidUsername('jboat', user_profile_collection);
       console.log(valid);
-  
+        
+      const pass = await db.getPassword('jboat', user_profile_collection);
+      console.log(pass);
+
     } finally {
       // Ensures that the client will close when you finish/error
       closeConnection(mongo_client);
