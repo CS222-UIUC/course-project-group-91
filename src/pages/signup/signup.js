0a6c3f288667082dbd1c6b993a96a9b45ae18c5c
Fay Lin <flin24@illinois.edu>
2022-11-04 14:05:07 -0500

    styled login/signup pages

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,34 +1,40 @@
 import React, { useState } from 'react'
 import "./signup.css"
 import TextField from "@material-ui/core/TextField"
+import Button from '@mui/material/Button'
+
+
 const SignUp = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     return(
-        <div className="mainContainer">
-            <div className="mainContainer">
+        <div className="body">
+            <div className="title">
                 <h1> SIGN UP </h1>
             </div>
-            <div className="mainContainer">
-                <div>
+            <div className = "signupForm">
+                <div className = "username" >
                     <TextField 
                     value={username}
-                    label="Enter your Username"
+                    label="Create Username"
                     onChange={(e) => {
                         setUsername(e.target.value);
                     }}/>
                 </div>
-                <div>
+                <div className = "password">
                     <TextField 
                     value={password}
-                    label="Enter your password"
+                    label="Create Password"
                     onChange={(e) => {
                         setPassword(e.target.value);
                     }}/>
                 </div>
-                <a href = "/"><button className='signUpButton'> Sign Up </button></a>
-                <a href="/login"> <h6> Already have an account? Log in </h6> </a>
-            </div>
+                <div className = "signupButton" >
+                    <Button variant="contained" type = "submit" > Sign Up </Button>
+                </div>
+                <div className = "accButton">
+                    <Button variant="outlined" href = "/login" >Already have an account? Log in</Button>
+                </div>            </div>
         </div>
     );
 }
