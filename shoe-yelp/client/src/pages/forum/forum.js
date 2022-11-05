a399729639a4bf423009cb5fa9f32509f6ccc9de
EmmanB1 <emmanallws@wirelessprv-10-192-199-38.near.illinois.edu>
2022-11-05 00:51:08 -0500

    Adding posts

--- shoe-yelp/client/src/pages/forum/forum.js
+++ shoe-yelp/client/src/pages/forum/forum.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import "./forum.css"
+import React, { useState } from 'react';
+import "./forum.css";
+import Post from "./post.js"
+import Button from '@mui/material/Button';
+
 const Forum = () => {
+  let data = [<div></div>]
+  const [posts, setPosts] = useState(data)
+  const addPost = event => {
+    console.log(posts)
+    let get_posts = posts.slice() // apparently we need to use .slice() to update posts
+    get_posts.push(<div ></div>)
+    setPosts(get_posts)
+  };
   return (
     <>
       <div className="body">
@@ -14,6 +25,16 @@ const Forum = () => {
             <h1>
               FORUM
             </h1>
+            <div>
+            {posts.map((post, index) =>{
+                return(
+                  <a href={"/forum/" + index}><Post post={post} path={"/forum/" + index}> </Post></a>
+                );
+            })}
+            
+            </div>
+            <Button variant="contained" onClick={addPost}>+</Button>
+            
             {/* include cards + create post button */}
           </div>
       </div>
